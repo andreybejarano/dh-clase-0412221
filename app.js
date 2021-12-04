1b04d575a169ea8c99b6fb1d68872b2d37cd37b3
Andrey Bejarano <abejarano@qubit.tv>
2021-12-04 12:47:15 -0300

    feat: base structure

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    return res.send('hola');
+    return res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, 'views/about.html'));
 });
 
 const port = 3000;
